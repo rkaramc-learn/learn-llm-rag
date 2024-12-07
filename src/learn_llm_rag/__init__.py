069bfc6f1b8958652f55e2f5736b165dd086f597
Rajeev Karamcheti <rkaramc@gmail.com>
2024-12-08 02:31:06 +0530

    step 1: call a simple LLM

--- src/learn_llm_rag/__init__.py
+++ src/learn_llm_rag/__init__.py
@@ -1,2 +1,26 @@
+from langchain_ollama import OllamaLLM
+
+
+def callSimpleLLM():
+    # Setup question
+    question = "What are the population projections for India for the next 50 years?"
+    print(f"Q: {question}")
+
+    # Initialize LLM with remote URL and available model
+    llm = OllamaLLM(
+        base_url="http://quark02.saury-gar.ts.net:11434",
+        model="llama3.2:latest",
+        format="json",
+    )
+
+    # Query LLM
+    llm_response = llm.invoke(question)
+    print(f"A: {llm_response.strip()}")
+
+
 def main():
-    print("Hello from rag!")
+    callSimpleLLM()
+
+
+if __name__ == "__main__":
+    main()
